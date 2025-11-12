01de959465742874a1882eb7774b98ed0d9c6ce5
haunt03 <haunthe176520@fpt.edu.vn>
2025-11-12 13:46:27 +0700

    add Employee module

---
+++ PTCMSS/ptcmss-backend/src/main/java/org/example/ptcmssbackend/repository/TripIncidentRepository.java
@@ -0,0 +1,4 @@
+package org.example.ptcmssbackend.repository;
+
+public class TripIncidentRepository {
+}
---
+++ PTCMSS/ptcmss-backend/src/main/java/org/example/ptcmssbackend/service/LocalImageService.java
@@ -0,0 +1,4 @@
+package org.example.ptcmssbackend.service;
+
+public class LocalImageService {
+}
