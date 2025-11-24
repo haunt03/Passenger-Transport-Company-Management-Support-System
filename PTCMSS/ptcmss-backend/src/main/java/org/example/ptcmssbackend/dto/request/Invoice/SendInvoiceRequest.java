bcac52b599dac8e046a1293a52f0453af53f2e36
haunt03 <haunthe176520@fpt.edu.vn>
2025-11-24 12:00:54 +0700

    Add DTOs for Debt and Invoice features

---
+++ PTCMSS/ptcmss-backend/src/main/java/org/example/ptcmssbackend/dto/request/Invoice/SendInvoiceRequest.java
@@ -0,0 +1,14 @@
+package org.example.ptcmssbackend.dto.request.Invoice;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class SendInvoiceRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    private String message;
+}
