ae4fc0d6cb2a26673088525b6cf0c483287620a2
Nguyễn Thúy Hậu <haunthe176520@fpt.edu.vn>
2025-11-28 15:00:58 +0700

    feat: add CustomerController, HireTypeController, PaymentConfirmationStatus and remove VerificationController

---
+++ PTCMSS/ptcmss-backend/src/main/java/org/example/ptcmssbackend/controller/CustomerController.java
@@ -0,0 +1,4 @@
+package org.example.ptcmssbackend.controller;
+
+public class CustomerController {
+}
---
+++ PTCMSS/ptcmss-backend/src/main/java/org/example/ptcmssbackend/enums/PaymentConfirmationStatus.java
@@ -0,0 +1,4 @@
+package org.example.ptcmssbackend.enums;
+
+public class PaymentConfirmationStatus {
+}
